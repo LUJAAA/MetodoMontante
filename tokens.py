a380bbf20963f0152f529dfb4dc73762256948c4
HombreLaser <sebastian-440@live.com>
2021-08-23 14:15:03 -0500

    Mejorada entrada de matrices

---
+++ tokens.py
@@ -0,0 +1,56 @@
+from matrices import Matriz
+from dataclasses import dataclass
+from enum import Enum
+
+
+class LexerException(Exception):
+    def __init__(self):
+        self.message = "Carácter inválido."
+
+
+class Tipo(Enum):
+    LEF_PARENS = 0
+    RIG_PARENS = 1
+    NUMERO = 2
+    COMA = 3
+
+
+@dataclass
+class Token:
+    tipo_token: Tipo
+    valor: str
+
+    def __str__(self):
+        return f"Tipo: {self.tipo_token} Valor: {self.valor}"
+
+
+
+class Tokenizador:
+    def __init__(self, cadena: str):
+        self.cadena = cadena.replace(' ', '')
+        self.puntero = 0
+
+    def formar_numero(self):
+        numero = ""
+
+        while self.cadena[self.puntero].isnumeric() and self.puntero < len(self.cadena):
+            numero += self.cadena[self.puntero]
+            self.puntero += 1
+
+        return numero
+
+    def tokenizar(self):
+        while self.puntero < len(self.cadena):
+            if self.cadena[self.puntero] == '[':
+                self.puntero += 1
+                yield Token(Tipo.LEF_PARENS, '[')
+            elif self.cadena[self.puntero] == ']':
+                self.puntero += 1
+                yield Token(Tipo.RIG_PARENS, ']')
+            elif self.cadena[self.puntero] == ',':
+                self.puntero += 1
+                yield Token(Tipo.COMA, ',')
+            elif self.cadena[self.puntero].isnumeric():
+                yield Token(Tipo.NUMERO, self.formar_numero())
+            else:
+                raise LexerException
